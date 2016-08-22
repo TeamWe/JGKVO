71fe995f4d1f1d62113723ab4c3d1c825c6ecafd
徐纪光 <jiguang@jiguangdemacbook-air.local>
2016-08-23 07:41:07 +0800

    Initial Commit

---
+++ JGKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JGKVO
+//
+//  Created by jiguang on 16/8/23.
+//  Copyright © 2016年 jiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JGKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JGKVO
+//
+//  Created by jiguang on 16/8/23.
+//  Copyright © 2016年 jiguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
